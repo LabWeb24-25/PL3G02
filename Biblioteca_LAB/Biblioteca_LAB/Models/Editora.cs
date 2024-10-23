30e11144e6c089ab32c465f6692abf7e5b1a4cde
David Fidalgo <160844915+davidfidalgo1004@users.noreply.github.com>
2024-10-23 13:14:52 +0100

    .

--- Biblioteca_LAB/Biblioteca_LAB/Models/Editora.cs
+++ Biblioteca_LAB/Biblioteca_LAB/Models/Editora.cs
@@ -5,17 +5,12 @@ namespace Biblioteca_LAB.Models
     public class Editora
     {
         [Key]
-        public int Id { get; set; }
-
+        public int ID { get; set; }
         [Required(ErrorMessage = "Required Field")]
-        [StringLength(120, ErrorMessage = "The {0} cannot exceed {1} characters.")]
+        [StringLength(120, ErrorMessage = "The {0} do not exceed {1} characteres.")]
         public string? Nome { get; set; }
-
-        // Info_Editora opciomal
-        [StringLength(5000, ErrorMessage = "The Info cannot exceed {1} characters.")]
+        [StringLength(5000, ErrorMessage = "The Info do not exceed {1} characteres.")]
         public string? Info { get; set; }
-
-        // Img_Editora opcional
         public string? IMG { get; set; }
     }
 }
--- Biblioteca_LAB/Biblioteca_LAB/Models/Pais.cs
+++ Biblioteca_LAB/Biblioteca_LAB/Models/Pais.cs
@@ -5,9 +5,9 @@ namespace Biblioteca_LAB.Models
     public class Pais
     {
         [Key]
-        public int Id { get; set; }
+        public int ID { get; set; }
         [Required(ErrorMessage = "Required Field")]
-        [StringLength(50, ErrorMessage = "The {0} cannot exceed {1} characteres.")]
-        public string? Nome { get; set; }
+        [StringLength(50, ErrorMessage = "The {0} do not exceed {1} characteres.")]
+        public string Nome { get; set; }
     }
 }
