b2523ea4cb791c1a5d6db886b7db1709cbea2d6d
David Fidalgo <160844915+davidfidalgo1004@users.noreply.github.com>
2024-11-23 14:49:38 +0000

    Add Controllers
    Autor
    Editora

---
+++ LibSpace/LibSpace/Controllers/AutoresController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LibSpace.Data;
+using LibSpace.Models;
+
+namespace LibSpace.Controllers
+{
+    public class AutoresController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AutoresController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Autores
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Autors.Include(a => a.IdLinguaNavigation);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Autores/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var autor = await _context.Autors
+                .Include(a => a.IdLinguaNavigation)
+                .FirstOrDefaultAsync(m => m.IdAutor == id);
+            if (autor == null)
+            {
+                return NotFound();
+            }
+
+            return View(autor);
+        }
+
+        // GET: Autores/Create
+        public IActionResult Create()
+        {
+            ViewData["IdLingua"] = new SelectList(_context.Set<Pais>(), "IdPais", "IdPais");
+            return View();
+        }
+
+        // POST: Autores/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdAutor,NomeAutor,DataNascimento,Pseudonimo,DataFalecimento,FotoAutor,Bibliografia,IdLingua")] Autor autor)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(autor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdLingua"] = new SelectList(_context.Set<Pais>(), "IdPais", "IdPais", autor.IdLingua);
+            return View(autor);
+        }
+
+        // GET: Autores/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var autor = await _context.Autors.FindAsync(id);
+            if (autor == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdLingua"] = new SelectList(_context.Set<Pais>(), "IdPais", "IdPais", autor.IdLingua);
+            return View(autor);
+        }
+
+        // POST: Autores/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdAutor,NomeAutor,DataNascimento,Pseudonimo,DataFalecimento,FotoAutor,Bibliografia,IdLingua")] Autor autor)
+        {
+            if (id != autor.IdAutor)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(autor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AutorExists(autor.IdAutor))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdLingua"] = new SelectList(_context.Set<Pais>(), "IdPais", "IdPais", autor.IdLingua);
+            return View(autor);
+        }
+
+        // GET: Autores/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var autor = await _context.Autors
+                .Include(a => a.IdLinguaNavigation)
+                .FirstOrDefaultAsync(m => m.IdAutor == id);
+            if (autor == null)
+            {
+                return NotFound();
+            }
+
+            return View(autor);
+        }
+
+        // POST: Autores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var autor = await _context.Autors.FindAsync(id);
+            if (autor != null)
+            {
+                _context.Autors.Remove(autor);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AutorExists(int id)
+        {
+            return _context.Autors.Any(e => e.IdAutor == id);
+        }
+    }
+}
--- LibSpace/LibSpace/Data/ApplicationDbContext.cs
+++ LibSpace/LibSpace/Data/ApplicationDbContext.cs
@@ -15,6 +15,7 @@ namespace LibSpace.Data
 		public DbSet<Biblioteca> Bibliotecas { get; set; }
 		public DbSet<Bloquear> Bloqueados { get; set; }
         public DbSet<LibSpace.Models.Livro> Livro { get; set; } = default!;
+        public DbSet<LibSpace.Models.Editora> Editora { get; set; } = default!;
 
 
 	}
