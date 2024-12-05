ecb821acda9ee1cc6f97f9880b90dffd0db3471d
Balente <tiago.valente.scarvalho@gmail.com>
2024-12-05 23:36:17 +0000

    Logout + Pagina Perfil a funceminar :p

--- LibSpace_Aspnet/Controllers/UsersController.cs
+++ LibSpace_Aspnet/Controllers/UsersController.cs
@@ -1,33 +1,180 @@
-using LibSpace_Aspnet.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using LibSpace_Aspnet.Data;
+using LibSpace_Aspnet.Models;
+using System.Security.Claims;
 
 namespace LibSpace_Aspnet.Controllers
 {
     public class UsersController : Controller
     {
         private readonly ApplicationDbContext _context;
-        public IActionResult Index()
+
+        public UsersController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
         }
-        // GETamos: Autors/Perfil
-        public async Task<IActionResult> Perfil(int? id)
+
+        // GET: Users
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Perfils.Include(p => p.EndCodPostalNavigation);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Users/Details/5
+        public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
 
-            var autor = await _context.Autors
-                .Include(a => a.IdLinguaNavigation)
-                .FirstOrDefaultAsync(m => m.IdAutor == id);
-            if (autor == null)
+            var perfil = await _context.Perfils
+                .Include(p => p.EndCodPostalNavigation)
+                .FirstOrDefaultAsync(m => m.IdPerfil == id);
+            if (perfil == null)
             {
                 return NotFound();
             }
 
-            return View(autor);
+            return View(perfil);
+        }
+
+        // GET: Users/Create
+        public IActionResult Create()
+        {
+            ViewData["EndCodPostal"] = new SelectList(_context.CodigoPostals, "EndCodPostal", "EndCodPostal");
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdPerfil,EndMorada,EndCodPostal,NomePerfil,DataNascimentoPerfil,Apelido,ImgPerfil,AspNetUserId")] Perfil perfil)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(perfil);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EndCodPostal"] = new SelectList(_context.CodigoPostals, "EndCodPostal", "EndCodPostal", perfil.EndCodPostal);
+            return View(perfil);
+        }
+
+        // GET: Users/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var perfil = await _context.Perfils.FindAsync(id);
+            if (perfil == null)
+            {
+                return NotFound();
+            }
+            ViewData["EndCodPostal"] = new SelectList(_context.CodigoPostals, "EndCodPostal", "EndCodPostal", perfil.EndCodPostal);
+            return View(perfil);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdPerfil,EndMorada,EndCodPostal,NomePerfil,DataNascimentoPerfil,Apelido,ImgPerfil,AspNetUserId")] Perfil perfil)
+        {
+            if (id != perfil.IdPerfil)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(perfil);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PerfilExists(perfil.IdPerfil))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EndCodPostal"] = new SelectList(_context.CodigoPostals, "EndCodPostal", "EndCodPostal", perfil.EndCodPostal);
+            return View(perfil);
+        }
+
+        // GET: Users/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var perfil = await _context.Perfils
+                .Include(p => p.EndCodPostalNavigation)
+                .FirstOrDefaultAsync(m => m.IdPerfil == id);
+            if (perfil == null)
+            {
+                return NotFound();
+            }
+
+            return View(perfil);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var perfil = await _context.Perfils.FindAsync(id);
+            if (perfil != null)
+            {
+                _context.Perfils.Remove(perfil);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PerfilExists(int id)
+        {
+            return _context.Perfils.Any(e => e.IdPerfil == id);
+        }
+
+        public async Task<IActionResult> Perfil()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            
+            var perfil = await _context.Perfils
+                .FirstOrDefaultAsync(p => p.AspNetUserId == userId);
+
+            if (perfil == null)
+            {
+                return NotFound();
+            }
+
+            return RedirectToAction(nameof(Details), new { id = perfil.IdPerfil });
         }
     }
 }
