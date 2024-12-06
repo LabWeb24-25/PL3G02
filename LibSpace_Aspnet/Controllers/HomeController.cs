64495939c66366bab868861556fb21a4bfc73485
Balente <tiago.valente.scarvalho@gmail.com>
2024-12-06 15:26:30 +0000

    Boogie Wonderland
    ta lindo

--- LibSpace_Aspnet/Controllers/HomeController.cs
+++ LibSpace_Aspnet/Controllers/HomeController.cs
@@ -1,21 +1,28 @@
 using LibSpace_Aspnet.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using LibSpace_Aspnet.Data;
 
 namespace LibSpace_Aspnet.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var featuredBooks = _context.Livros
+                .Include(l => l.IdAutorNavigation)
+                .Take(5)  // Limit to 5 books or adjust as needed
+                .ToList();
+            
+            return View(featuredBooks);
         }
 
         public IActionResult Privacy()
